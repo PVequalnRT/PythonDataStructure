a3221d9f79437a6392794a1391450b10120b7876
PVequalnRT <usetino913@gmail.com>
2019-07-12 16:28:11 +0900

    create searchNode class, push and pop method

--- stack/stack.py
+++ stack/stack.py
@@ -2,3 +2,21 @@ class stackNode:
     def __init__(self,data):
         self.data = data
         self.downNode = None
+
+class searchNode:
+    def __init__(self):
+        self.top = None
+    def push(self, data):
+        if self.top == None:
+            self.top = stackNode(data)
+        else:
+            temp = stackNode(data)
+            temp.downNode = self.top
+            self.top = temp
+    def pop(self):
+        if self.top == None:
+            return "No Data!"
+        else:
+            temp = self.top
+            self.top = temp.downNode
+            return temp.data
