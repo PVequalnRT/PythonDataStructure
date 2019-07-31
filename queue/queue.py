6192837db3939c4a80db3b0609c5238a8e79c2cf
PVequalnRT <usetino913@gmail.com>
2019-07-31 16:41:44 +0900

    create CUI menu

--- queue/queue.py
+++ queue/queue.py
@@ -1,1 +1,29 @@
+class queueNode:
+    def __init__(self):
+        self.data = None
+        self.nextNode = None
 
+class searchNode:
+    def __init__(self):
+        pass
+
+selectMenu = """1.데이터 추가
+2.데이터 삭제
+3.데이터 전체 출력
+4.프로그램 종료
+"""
+
+node = searchNode()
+while True:
+    print(selectMenu)
+    menu = int(input("메뉴를 선택해주세요 : "))
+    if menu == 1:
+        node.push(data)
+    elif menu == 2:
+        node.pop()
+    elif menu == 3:
+        node.popAll()
+    elif menu == 4:
+        print("프로그램을 종료합니다.")
+        break
+        
