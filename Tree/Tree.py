da13a7acd9352c1d8cd67ab39008927e647d5972
PVequalnRT <usetino913@gmail.com>
2019-08-02 17:00:02 +0900

    Create Node Search Class

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -3,3 +3,30 @@ class node:
         self.data = data
         self.rightNode = None
         self.leftNode = None
+
+class nodeSearch:
+    pass
+
+
+menuIndex = """1.데이터 추가
+2.가장 큰 값 출력
+3.가장 작은 값 출력
+4.내림차순 전체출력
+5.오름차순 전체출력
+6.종료."""
+
+menu = int(input("\n메뉴를 선택해주세요 : "))
+rootNode = nodeSearch()
+
+if menu == 1:
+    pass
+elif menu == 2:
+    pass
+elif menu == 3:
+    pass
+elif menu == 4:
+    pass
+elif menu == 5:
+    pass
+elif menu == 6:
+    pass
