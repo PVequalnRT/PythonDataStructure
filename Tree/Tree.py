31d2560b284a73e93f5868e2fa0b70496deb516a
PVequalnRT <usetino913@gmail.com>
2019-08-05 14:57:31 +0900

    Create search Biggest Data in Tree

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -26,10 +26,21 @@ class nodeSearch:
                     else:
                         temp = temp.leftNode
 
+    def biggestData(self):
+        def searchRightNode(temp):
+            if temp.rightNode == None:
+                print("가장 큰 값 : %d"%temp.data)
+            else:
+                searchRightNode(temp.rightNode)
+        if self.root == None:
+            return "No Data!"
+        else:
+            searchRightNode(self.root)
+
 menuIndex = """1.데이터 추가
 2.가장 큰 값 출력
-3.데이터 찾기
-4.가장 작은 값 출력
+3.가장 작은 값 출력
+4.데이터 찾기
 5.내림차순 전체출력
 6.오름차순 전체출력
 7.종료."""
@@ -44,7 +55,7 @@ while True:
         data = int(input("추가할 데이터 : "))
         root.push(data)
     elif menu == 2:
-        pass
+        root.biggestData()
     elif menu == 3:
         pass
     elif menu == 4:
